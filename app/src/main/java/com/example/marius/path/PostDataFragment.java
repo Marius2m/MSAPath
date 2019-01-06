9d736066bc7fcec35cc9228941b234f74c904d18
Marius Mircea <mariusmircea@outlook.com>
2019-01-06 23:16:56 +0200

    Can add many paragraphs now
    Scrolling does not currently work. Data to firebase is not yet uploaded.

--- app/src/main/java/com/example/marius/path/PostDataFragment.java
+++ app/src/main/java/com/example/marius/path/PostDataFragment.java
@@ -1,10 +1,9 @@
 package com.example.marius.path;
 
 import android.app.AlertDialog;
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -16,20 +15,35 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.marius.path.user_data.PostData;
 
-import org.w3c.dom.Text;
-
 
 public class PostDataFragment extends Fragment {
+    private ScrollView scrollView;
+    private TextView test;
+    private RelativeLayout parentRLayout;
+    private CoordinatorLayout coordinatorLayout;
+
+    private int currentId = 0;
+    private int lineIndex = 0;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_post_data, container, false);
 
+        //scrollView = (ScrollView) v.findViewById(R.id.scrollViewId);
+        parentRLayout = (RelativeLayout) v.findViewById(R.id.relativeLayoutId);
+        coordinatorLayout = (CoordinatorLayout) v.findViewById(R.id.coordinatorLayoutId);
+        test = (TextView) v.findViewById(R.id.row0);
+        currentId = test.getId();
+
+        Log.d("currentId test", ""+currentId);
         final FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.fabtn);
         final FloatingActionButton picBtn = (FloatingActionButton) v.findViewById(R.id.fabPicture);
         FloatingActionButton textBtn = (FloatingActionButton) v.findViewById(R.id.fabText);
@@ -43,6 +57,7 @@ public class PostDataFragment extends Fragment {
         final Animation hideBtn = AnimationUtils.loadAnimation(getActivity(), R.anim.hide_button);
         final Animation showLayout = AnimationUtils.loadAnimation(getActivity(), R.anim.show_layout);
         final Animation hideLayout = AnimationUtils.loadAnimation(getActivity(), R.anim.hide_layout);
+
 
         fab.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -93,8 +108,10 @@ public class PostDataFragment extends Fragment {
                     public void onClick(View v) {
                         if(!paragraphText.getText().toString().isEmpty()){
                             Log.d("Dialog: text is: ", paragraphText.getText().toString());
-                            Toast.makeText(getActivity(), paragraphText.getText().toString(), Toast.LENGTH_SHORT).show();
-                           // createNewTextView(paragraphText.getText().toString(), alignment, margin, padding);
+                           // Toast.makeText(getActivity(), paragraphText.getText().toString(), Toast.LENGTH_SHORT).show();
+
+
+                            parentRLayout.addView(createNewTextView(paragraphText.getText().toString(), 0, 5, 5));
                             dialog.dismiss();
                         }else{
                             Toast.makeText(getActivity(), "Empty", Toast.LENGTH_SHORT).show();
@@ -108,8 +125,6 @@ public class PostDataFragment extends Fragment {
                         dialog.dismiss();
                     }
                 });
-
-                Log.d("InsideText", "TEXT");
             }
         });
 
@@ -127,16 +142,33 @@ public class PostDataFragment extends Fragment {
             }
         });
 
-        //Bundle b = getArguments();
+        Bundle b = getArguments();
         PostData postData = (PostData) getArguments().getSerializable("PostData");
         Log.i("getPostData",postData.toString());
 
         return v;
     }
 
-//    TextView createNewTextView(String text, int alignment, int margin, int padding){
-//        TextView newDynamicTextView = new TextView(getContext());
-//
-//
-//    }
+    TextView createNewTextView(String text, int alignment, int margin, int padding){
+        TextView newDynamicTextView = new TextView(getActivity());
+
+        /*String idName = "row"+lineIndex;
+        int lineId = Integer.parseInt(idName);
+        newDynamicTextView.setId(lineId);
+*/
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.addRule(RelativeLayout.BELOW, currentId);
+        newDynamicTextView.setLayoutParams(layoutParams);
+
+        newDynamicTextView.setId(TextView.generateViewId());
+
+        currentId = newDynamicTextView.getId();
+        Log.d("currentId", ""+currentId);
+        newDynamicTextView.setText(text);
+
+        Log.d("newDynamic:", newDynamicTextView.getText().toString());
+        return newDynamicTextView;
+    }
 }
