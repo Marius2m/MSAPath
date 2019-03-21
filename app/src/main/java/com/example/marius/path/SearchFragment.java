2fe2bb2104a034f3d0532322c38986be6c859ad2
Marius Mircea <mariusmircea@outlook.com>
2019-03-21 18:32:20 +0200

    Setup Maps + Map Activity

--- app/src/main/java/com/example/marius/path/SearchFragment.java
+++ app/src/main/java/com/example/marius/path/SearchFragment.java
@@ -1,5 +1,6 @@
 package com.example.marius.path;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,7 +13,7 @@ import android.view.ViewGroup;
 public class SearchFragment extends Fragment {
     private View v;
     private CardView cardView;
-
+    private CardView cardViewMap;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
@@ -27,6 +28,19 @@ public class SearchFragment extends Fragment {
                 fragmentT.replace(R.id.fragment_container, new AllPostsFragment()).commit();
             }
         });
+
+        cardViewMap = v.findViewById(R.id.cardViewMap);
+        cardViewMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getContext(), MapGlobeActivity.class);
+                startActivity(intent);
+//                FragmentTransaction fragmentT = getFragmentManager().beginTransaction();
+//                fragmentT.replace(R.id.map, new AllPostsFragment()).commit();
+            }
+        });
+
 
         //return inflater.inflate(R.layout.fragment_search, container, false);
         return v;
