53233d5c81f36839af608e2d4bff13b0323f1549
Marius Mircea <mariusmircea@outlook.com>
2019-04-06 23:18:12 +0300

    Added Google Maps Location Suggestion
    Extracted Data from the Queried Location,
    Displayed formatted data on AddFragment Screen,
    
    To do:
    get+set regionID (on server side),
    upload data to firebase

--- app/src/main/java/com/example/marius/path/AddFragment.java
+++ app/src/main/java/com/example/marius/path/AddFragment.java
@@ -14,26 +14,42 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.MimeTypeMap;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.marius.path.user_data.PostData;
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.TypeFilter;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static android.app.Activity.RESULT_OK;
+import static com.example.marius.path.FieldSelector.getPlaceFields;
 
 public class AddFragment extends Fragment {
     private static final int PICK_IMG_REQ_CODE = 1;
+    private static final int AUTOCOMPLETE_REQUEST_CODE = 23487;
 
     private EditText postTitle, postLocation, postNrOfTravelers, postDate;
     private Button submitDataBtn;
@@ -49,9 +65,15 @@ public class AddFragment extends Fragment {
     ContentResolver contentResolver;
     MimeTypeMap mime;
     private Uri coverPhotoUri;
-
+    private PlacesClient placesClient;
 
     private PostData postData = null;
+
+    private String location = "";
+    private String longitude = "";
+    private String latitude = "";
+    private String city = "";
+    private String country = "";
 
     @Nullable
     @Override
@@ -67,6 +89,43 @@ public class AddFragment extends Fragment {
         coverPhotoImgView = (ImageView) v.findViewById(R.id.coverPhotoImgView);
         headerTitle = (TextView) v.findViewById(R.id.headerTitle);
 
+        //START
+
+
+        String apiKey = "AIzaSyD7TC1decqiw1iKCFjaoggsraaruxTXtPE";
+
+//        if (apiKey.equals("")) {
+//            Toast.makeText(this, getString(R.string.error_api_key), Toast.LENGTH_LONG).show();
+//            return;
+//        }
+
+        // Setup Places Client
+        if (!Places.isInitialized()) {
+            Places.initialize(this.getContext(), apiKey);
+        }
+        // Retrieve a PlacesClient (previously initialized - see MainActivity)
+        placesClient = Places.createClient(this.getContext());
+
+        // Set up view objects
+        Spinner typeFilterSpinner = v.findViewById(R.id.autocomplete_type_filter);
+        typeFilterSpinner.setAdapter(
+                new ArrayAdapter<>(
+                        this.getContext(), android.R.layout.simple_list_item_1, Arrays.asList(TypeFilter.values())));
+
+        CheckBox useTypeFilterCheckBox = v.findViewById(R.id.autocomplete_use_type_filter);
+        useTypeFilterCheckBox.setOnCheckedChangeListener(
+                (buttonView, isChecked) -> typeFilterSpinner.setEnabled(isChecked));
+
+        // Set listeners for Autocomplete activity
+        v.findViewById(R.id.postLocation)
+                .setOnClickListener(view -> startAutocompleteActivity());
+
+        // UI initialization
+        typeFilterSpinner.setEnabled(false);
+
+
+
+        ////DONE
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
@@ -86,14 +145,14 @@ public class AddFragment extends Fragment {
             }
         });
 
-        postLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "Tapped", Toast.LENGTH_SHORT).show();
-                postLocation.setText("Romania!!!");
-                System.out.println("Tapped Where was this");
-            }
-        });
+//        postLocation.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getActivity(), "Tapped", Toast.LENGTH_SHORT).show();
+//                postLocation.setText("Romania!!!");
+//                System.out.println("Tapped Where was this");
+//            }
+//        });
 
 
         /*nextPageBtn = (Button) v.findViewById(R.id.nextPage);
@@ -208,21 +267,81 @@ public class AddFragment extends Fragment {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        coverPhotoBtn.setText("  Change cover image");
-        headerTitle.setText("");
 
-        if(requestCode == PICK_IMG_REQ_CODE && resultCode == RESULT_OK
-                && data != null && data.getData() != null){
-            coverPhotoUri = data.getData();
+        if (requestCode == PICK_IMG_REQ_CODE) {
+            super.onActivityResult(requestCode, resultCode, data);
+            coverPhotoBtn.setText("  Change cover image");
+            headerTitle.setText("");
 
-            Log.d("coverPhotoUri:", coverPhotoUri.toString());
-            coverPhotoImgView.setColorFilter(Color.argb(67,14,13,14));
-            Picasso.get()
-                    .load(coverPhotoUri)
-                    .centerCrop()
-                    .fit()
-                    .into(coverPhotoImgView);
+            if (requestCode == PICK_IMG_REQ_CODE && resultCode == RESULT_OK
+                    && data != null && data.getData() != null) {
+                coverPhotoUri = data.getData();
+
+                Log.d("coverPhotoUri:", coverPhotoUri.toString());
+                coverPhotoImgView.setColorFilter(Color.argb(67, 14, 13, 14));
+                Picasso.get()
+                        .load(coverPhotoUri)
+                        .centerCrop()
+                        .fit()
+                        .into(coverPhotoImgView);
+            }
+        }
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == AutocompleteActivity.RESULT_OK) {
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                String locationData = StringUtil.stringifyAutocompleteWidget(place, true);
+                Log.d("locationData", locationData);
+                setLocationField(locationData);
+
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                Status status = Autocomplete.getStatusFromIntent(data);
+                Toast.makeText(getContext(), "Please select the location again", Toast.LENGTH_SHORT).show();
+                postLocation.setText(status.getStatusMessage());
+            } else if (resultCode == AutocompleteActivity.RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+            // Required because this class extends AppCompatActivity which extends FragmentActivity
+            // which implements this method to pass onActivityResult calls to child fragments
+            // (eg AutocompleteFragment).
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+     }
+
+    private void setLocationField(String locationData) {
+        String regex = "address=([A-Za-z,\\u00BF-\\u1FFF\\u2C00-\\uD7FF\\w -]*)attributions[a-zA-Z =\\[\\],0-9-]*lat/lng:\\W*([0-9\\.]*),([0-9\\.]*)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(locationData);
+
+        while(matcher.find()) {
+            location = matcher.group(1);
+            longitude = matcher.group(2);
+            latitude = matcher.group(3);
+        }
+
+        location = location.replaceAll(" ", "");
+        String[] res = location.split(",");
+
+        if(res.length >= 2) {
+            city = res[res.length - 2];
+            country = res[res.length - 1];
+            System.out.println(city);
+            System.out.println(country);
+            postLocation.setText(city + ", " + country);
+        }else if(res.length == 1) {
+            country = res[res.length - 1];
+            System.out.println(country);
+            postLocation.setText(country);
         }
     }
+
+    // Google Maps Location Suggestions
+    private void startAutocompleteActivity() {
+        Intent autocompleteIntent =
+                new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, getPlaceFields())
+                        //.setTypeFilter(getTypeFilter())
+                        .build(this.getContext());
+        startActivityForResult(autocompleteIntent, AUTOCOMPLETE_REQUEST_CODE);
+    }
+
 }
